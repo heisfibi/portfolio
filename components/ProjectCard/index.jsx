96deab23c3d0020a638d70958683abdf55427466
Aidan <datbluemushroom@gmail.com>
2022-07-27 02:33:43 -0400

    🩹 Project Card Tweaks

--- components/ProjectCard/index.jsx
+++ components/ProjectCard/index.jsx
@@ -4,7 +4,7 @@ export const ProjectCard = ({ title, description, image }) =>
 (
     <>
     
-        <div className='flex flex-col items-center w-[30em] h-full bg-almost-black-100 rounded-2xl drop-shadow-md transition-all duration-300 hover:scale-95 hover:drop-shadow-2xl'>
+        <div className='flex flex-col items-center w-[30em] h-full bg-almost-black-100 rounded-2xl drop-shadow-md transition-all duration-300 hover:scale-95 hover:drop-shadow-2xl cursor-pointer'>
             <img className='rounded-t-2xl w-full h-[20em] object-cover' src={image}/>
             <div className='flex flex-col p-10 gap-7'>
                 <h1 className='text-4xl font-semibold'>{title}</h1>
