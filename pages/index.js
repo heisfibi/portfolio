adccf0d5be78241dead692c60a286af5f18a4b96
Aidan <datbluemushroom@gmail.com>
2022-07-27 02:31:23 -0400

    🎨 Customized Project Cards

--- pages/index.js
+++ pages/index.js
@@ -36,6 +36,24 @@ export default function Home()
           <div className='bg-white w-1/2 h-1.5 px-16'></div>
           <h1 className='text-white text-4xl font-semibold py-16'>Featured Projects</h1>
         </div>
+
+        <div className='flex flex-row justify-evenly px-20'>
+          <ProjectCard 
+            title="Stevens '26 Student Portfolio Website"
+            description="Working as Project Manager of a student portfolio website a group of my classmates are working on. It's my first time working with others on a large project so I'm learning a lot!"
+            image="https://web.stevens.edu/news/newspoints/zoom-bg/Stevens-zoom-background-garden.jpg"
+          />
+          <ProjectCard 
+            title="Laptop Upcycle G-Suite Registration System"
+            description="Currently a software engineer intern for a non-profit technology organization. Building a registration system for volunteers to sign-up and attent volunteer sessions."
+            image="https://techcrunch.com/wp-content/uploads/2020/10/google-workspace-icons.jpg"
+          />
+          <ProjectCard 
+            title="Java Text-Based Game"
+            description="More of a personal project. Making a silly little Java text based game with weapons and a character based map. Going back to step one of all coders journey into programming!"
+            image="https://media.wired.com/photos/5f7b9cfd126693e1dc256c39/4:3/w_1800,h_1350,c_limit/games-settings-wired.png"
+          />
+        </div>
       </div>
     </>
 
