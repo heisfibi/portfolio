54a634e7422ad7c0e5f362700649b314025e80d8
Aidan <datbluemushroom@gmail.com>
2022-07-27 02:23:24 -0400

    🚀 Project Card Component

--- components/InfoCard/index.jsx
+++ components/InfoCard/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-export const InfoCard = ({ title, description, image }) =>
+export const InfoCard = ({ title, description, image, emoji }) =>
 (
     <>
     
         <div className='flex flex-row items-center w-2/3 h-[20em] bg-almost-black-500 rounded-2xl drop-shadow-md transition-all duration-300 hover:-translate-y-5 hover:drop-shadow-2xl'>
-            <img className='rounded-l-2xl h-full' src='https://i.ibb.co/6mGbqWv/122426423-2630232447290059-1245205322917720626-n.jpg'/>
+            <img className='rounded-l-2xl h-full' src={image}/>
             <div className='flex flex-col p-10 gap-7'>
                 <h1 className='text-4xl font-semibold'>{title}</h1>
                 <p className='text-xl'>{description}</p>
             </div>
-            <img className='w-1/2 h-1/2 pr-14' src={image}/>
+            <img className='w-1/2 h-1/2 pr-14' src={emoji}/>
         </div>
     
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { InfoCard } from '../components/InfoCard';
+import { ProjectCard } from '../components/ProjectCard';
 
 /*
   Emojis: https://emoji.craftwork.design/
@@ -20,18 +21,20 @@ export default function Home()
         <InfoCard 
           title="Who is this?"
           description="Hi! My name is Aidan Ouckama and I'm a upcoming Computer Science student and front-end developer. I have touched many corners within the programming world including Web Design, Game Development, Database Building, Modding, and more! I've had a passion for coding from a young age and I'm excited to continuing pursuing it!"
-          image='https://emoji.craftwork.design/images/modal/thinking-face.png'
+          image='https://i.ibb.co/6mGbqWv/122426423-2630232447290059-1245205322917720626-n.jpg'
+          emoji='https://emoji.craftwork.design/images/modal/thinking-face.png'
         />
       </div>
 
-      <div className='flex flex-row justify-center text-gold text-8xl font-bold pb-20'>
+      <div className='flex flex-row justify-center text-gold text-7xl font-bold pb-20'>
         <h1 className='text-almost-black-500 text-8xl font-bold'>“</h1><h1>Hi. I'm Aidan 👋</h1><h1 className='text-almost-black-500 text-8xl font-bold'>”</h1>
       </div>
 
-      <div className='w-full h-[35em] bg-almost-black-500'>
-        <div className='flex flex-row justify-center items-center gap-16'>
+      <div className='w-full h-[70em] bg-almost-black-500'>
+        <div className='flex flex-row justify-center items-center gap-16 pt-10 pb-16'>
+          <img className='w-[10em] h-[10em]' src='https://emoji.craftwork.design/images/modal/face-with-open-mouth.png'/>
+          <div className='bg-white w-1/2 h-1.5 px-16'></div>
           <h1 className='text-white text-4xl font-semibold py-16'>Featured Projects</h1>
-          <div className='bg-white w-3/4 h-1.5 pr-16'></div>
         </div>
       </div>
     </>
