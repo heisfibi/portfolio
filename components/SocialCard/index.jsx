491de6ff86d980d34629572757222b74c7cbfe68
Richard Iyoke <66504142+heisfibi@users.noreply.github.com>
2023-05-17 17:51:41 +0100

    Update index.jsx

--- components/SocialCard/index.jsx
+++ components/SocialCard/index.jsx
@@ -4,7 +4,7 @@ export const SocialCard = ({ title, description, image, link }) =>
     <>
         <a href={link} target="_blank" rel="noreferrer">
             <div className='flex flex-col lg:h-60 lg:flex-row items-center w-full h-full bg-almost-black-100 rounded-2xl drop-shadow-md transition-all duration-300 hover:-translate-y-3 sm:hover:-translate-y-5 hover:drop-shadow-2xl'>
-                <img className='lg:rounded-l-2xl bg-white rounded-t-2xl sm:rounded-t-none w-full lg:w-fit sm:aspect-square h-60 object-cover' src={image}/>
+                <img className='lg:rounded-l-2xl bg-white rounded-t-2xl lg:rounded-r-none w-full lg:w-3/6 lg:h-full max-h-64 lg:aspect-square aspect-[4/3] object-cover' src={image}/>
                 <div className='flex flex-col justify-center w-full h-full p-3 pb-4 sm:p-6 gap-4'> 
                     <h1 className='text-3xl font-semibold h-1/2 text-white'>{title}</h1>
                     <p className='text-base text-gray-300 h-30'>{description}</p>
